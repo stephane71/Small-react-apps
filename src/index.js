3df564cc9525c2b65206bc1eedece58c0eaac30d
stephane <stephanemaire71@gmail.com>
2022-05-01 14:45:12 +0200

    index | update ReactDOM call

--- src/index.js
+++ src/index.js
@@ -1,16 +1,14 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import React from "react";
+import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
-
 import App from "./App";
 
 const rootElement = document.getElementById("root");
-const root = createRoot(rootElement);
 
-root.render(
+const app = (
   <BrowserRouter>
-    <StrictMode>
-      <App />
-    </StrictMode>
+    <App />
   </BrowserRouter>
 );
+
+ReactDOM.render(app, rootElement);
