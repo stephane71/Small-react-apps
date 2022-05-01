c454a4cdb41cf1a3c183b9e70ab6e6a76fd9f9b2
stephane <stephanemaire71@gmail.com>
2022-05-01 15:39:19 +0200

    Index | add CssBaseline from Mui

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
+import CssBaseline from "@mui/material/CssBaseline";
 import App from "./App";
 
 const rootElement = document.getElementById("root");
 
 const app = (
   <BrowserRouter>
+    <CssBaseline />
     <App />
   </BrowserRouter>
 );
