308c6d33b94aa5ff137a268a5957b48b174b86c9
stephane <stephanemaire71@gmail.com>
2022-05-01 15:06:00 +0200

    App | update Home link

--- src/App.js
+++ src/App.js
@@ -1,18 +1,34 @@
 import "./styles.css";
 import ShoppingList from "./ShoppingList";
 import Home from "./Home";
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route, useNavigate } from "react-router-dom";
+import HomeIcon from "@mui/icons-material/Home";
+import { Chip, Box } from "@mui/material";
 
 export default function App() {
+  const navigate = useNavigate();
+
+  function handleClickHome() {
+    navigate("/");
+  }
+
   return (
     <div className="App">
-      <Link to="/">
-        <h1 className="appTitle">Exercices ReatJS - semaine 16</h1>
-      </Link>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/shopping-list" element={<ShoppingList />} />
-      </Routes>
+      <h1 className="appTitle">App of Small Apps</h1>
+
+      <Chip
+        icon={<HomeIcon />}
+        label="Home"
+        variant="outlined"
+        onClick={handleClickHome}
+      />
+
+      <Box sx={{ mt: 2 }}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/shopping-list" element={<ShoppingList />} />
+        </Routes>
+      </Box>
     </div>
   );
 }
