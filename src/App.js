cb9f3673958c9d62e9a19f0a7fb804e08f73e22f
stephane <stephanemaire71@gmail.com>
2022-05-01 10:05:27 +0200

    Init app with router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import "./styles.css";
 import ShoppingList from "./ShoppingList";
+import Home from "./Home";
+import { Routes, Route, Link } from "react-router-dom";
 
 export default function App() {
   return (
     <div className="App">
-      <h1 className="appTitle">Exercices ReatJS - semaine 16</h1>
-      <ShoppingList />
+      <Link to="/">
+        <h1 className="appTitle">Exercices ReatJS - semaine 16</h1>
+      </Link>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/shopping-list" element={<ShoppingList />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/Home.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import Box from "@mui/material/Box";
+
+class Home extends React.Component {
+  render() {
+    return (
+      <Box sx={{ display: "flex", flexDirection: "column" }}>
+        <Link to="/shopping-list">Shopping list</Link>
+        <Link to="/weather">Météo</Link>
+      </Box>
+    );
+  }
+}
+
+export default Home;
